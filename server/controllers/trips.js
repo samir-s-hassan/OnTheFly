d7334c6441ef49b88aecf5374a7fe8235a317f14
samir-s-hassan <hsamir347@gmail.com>
2024-11-04 21:15:53 -0500

    added function for getting one trip

--- server/controllers/trips.js
+++ server/controllers/trips.js
@@ -28,7 +28,20 @@ const getTrips = async (req, res) => {
   }
 };
 
+const getTrip = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const results = await pool.query("SELECT * FROM trips WHERE id = $1", [id]);
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+    console.log("Unable to get trip");
+    console.log("Error:", error.message);
+  }
+};
+
 export default {
   createTrip,
   getTrips,
+  getTrip,
 };
