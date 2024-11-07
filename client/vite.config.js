fcfe9eef993c89ec43e3b4a26f4c7444667d3038
samir-s-hassan <hsamir347@gmail.com>
2024-11-07 14:43:48 -0500

    after lots of changes, able to npm start now

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001'
-      }
-    }
-  }
-})
+        target: 'http://localhost:3001',
+      },
+    },
+  },
+});
