614bbf7895ee59bb07b979f80522551016926141
samir-s-hassan <hsamir347@gmail.com>
2024-11-10 12:00:09 -0500

    can now update number of votes for an activity

--- client/src/components/ActivityBtn.jsx
+++ client/src/components/ActivityBtn.jsx
@@ -1,20 +1,37 @@
-import React, {useState} from 'react';
-import './ActivityBtn.css'
+import React, { useState } from "react";
+import "./ActivityBtn.css";
 
-const ActivityBtn = (props) =>  {
+// Component for displaying an activity button with upvote functionality
+const ActivityBtn = (props) => {
+  // State to store the current number of votes for the activity
+  const [num_votes, setNumVotes] = useState(props.num_votes);
 
-  const [num_votes, setNumVotes] = useState(props.num_votes)
-
+  // Function to handle updating the vote count
   const updateCount = () => {
+    // Set up the PATCH request options to update the vote count on the server
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Increment the current number of votes by 1 and send it in the request body
+      body: JSON.stringify({ num_votes: num_votes + 1 }),
+    };
 
-  }
+    // Make a PATCH request to update the vote count for the activity with the given ID
+    fetch("/api/activities/" + props.id, options);
+
+    // Update the local state to reflect the new vote count
+    setNumVotes((num_votes) => num_votes + 1);
+  };
 
   return (
-    <button className='activityBtn' id={props.id} onClick={updateCount}>
-      {props.activity} <br/> {'△ ' + num_votes + ' Upvotes' }
+    // Render a button with the activity name and vote count
+    <button className="activityBtn" id={props.id} onClick={updateCount}>
+      {/* Display the activity name and the current number of upvotes */}
+      {props.activity} <br /> {"△ " + num_votes + " Upvotes"}
     </button>
-  )
-
-}
+  );
+};
 
 export default ActivityBtn;
